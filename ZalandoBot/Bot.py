b0add84d7fcf2493bbeb0438bff152a8166bbeaf
Student235281 <235281@student.edu.wroc.pl>
2020-02-03 21:35:16 +0100

    fixes

--- ZalandoBot/Bot.py
+++ ZalandoBot/Bot.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-import os
 import time
+
 
 class ZalandoBot:
     def __init__(self, username, password):
@@ -15,12 +15,14 @@ class ZalandoBot:
         email.send_keys(self.username)
         password = self.driver.find_element_by_xpath('//*[@id="form-password"]')
         password.send_keys(self.password)
-        click = self.driver.find_element_by_xpath('//*[@id="react-root-form"]/div/div/div[4]/div/form/fieldset/button').click()
+        click = self.driver.find_element_by_xpath('//*[@id="react-root-form"]/div/div/div[4]/div/form/fieldset/button')
+        click.click()
 
-    def open_campagnies(self, campagnies_id):
+    def open_companies(self, campagnies_id):
         self.driver.get('https://www.zalando-lounge.pl/campaigns/'+campagnies_id+'/all')
+
 
 ig_bot = ZalandoBot('nasze.zdjecia1997@gmail.com', 'kubekpatryk')
 ig_bot.login()
-time.sleep(5)
-ig_bot.open_campagnies('ZZO0Y5U')
+time.sleep(2)
+ig_bot.open_companies('ZZO0Y5U')
