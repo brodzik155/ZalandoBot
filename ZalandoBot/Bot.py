9f17baaab0e19329e4ab0ad510b238a99ea859fe
Patryk Brodziak <patryk334@vp.pl>
2020-02-04 19:59:43 +0100

    select not works...

--- ZalandoBot/Bot.py
+++ ZalandoBot/Bot.py
@@ -1,27 +1,36 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 import time
 
+categories = '//*[@id="inner-wrapper"]/section/div[2]/nav/a[1]/div'
+credentials_validate = '//*[@id="react-root-form"]/div/div/div[4]/div/form/fieldset/button'
+passwd = '//*[@id="form-password"]'
+log = '//*[@id="form-email"]'
 
 class ZalandoBot:
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        #option which let you open in full size
-        options = webdriver.ChromeOptions()
-        options.add_argument("--start-maximized")
-        self.driver = webdriver.Chrome('chromedriver.exe', options=options)
+        self.driver = webdriver.Chrome('chromedriver.exe', options=self.full_size())
         self.driver.get('https://www.zalando-lounge.pl/#/login')
 
+#BEDE PISAŁ PO POLSKU< ZEBY LATWIEJ BYŁO XD - dorobilem tutaj metode statyczną, zeby robila pełny ekran.
+    #SPRAWDZONE = DZIAŁA
+    @staticmethod
+    def full_size():
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")
+        return options
+# CO DO TYCH ZMIENNYCH TO FINALNIE PIERDOLNIE SIE JAKIS SŁOWNIK I BEDZIE SOBIE WYCIAGALO ZE SŁOWNIKA ODPOWIEDNIE WARTOSĆI, JAKIE BEDA POTRZEBNE.     A CO!
     def login(self):
-        email = self.driver.find_element_by_xpath('//*[@id="form-email"]')
+        email = self.driver.find_element_by_xpath(log)
         email.send_keys(self.username)
-        password = self.driver.find_element_by_xpath('//*[@id="form-password"]')
+        password = self.driver.find_element_by_xpath(passwd)
         password.send_keys(self.password)
-        click = self.driver.find_element_by_xpath('//*[@id="react-root-form"]/div/div/div[4]/div/form/fieldset/button')
-        click.click()
+        submit = self.driver.find_element_by_xpath(credentials_validate)
+        submit.click()
         time.sleep(1)
-        self.driver.find_element_by_xpath('//*[@id="mainNavWrapper"]/div[2]/div[2]/svg').click()
 
     def open_companies(self, campagnies_id):
         self.driver.get('https://www.zalando-lounge.pl/campaigns/'+campagnies_id+'/all')
@@ -29,16 +38,20 @@ class ZalandoBot:
     def close(self):
         self.driver.quit()
 
-    def proba(self):
-        self.driver.find_element_by_xpath('//*[@id="inner-wrapper"]/section/div[2]/nav/a[1]/div').click()
-
+    def choose_categories(self):
+        self.driver.find_element_by_xpath(categories).click()
+        time.sleep(1)   #Odtad nie działa....
+        select = Select(self.driver.find_element_by_xpath(categories))
+        time.sleep(1)
+        select.select_by_visible_text('Spodnie')
 
 ig_bot = ZalandoBot('nasze.zdjecia1997@gmail.com', 'kubekpatryk')
 try:
     ig_bot.login()
     time.sleep(2)
-    ig_bot.open_companies('ZZO0Y5U')
-    time.sleep(1)
-    ig_bot.proba()
+    ig_bot.open_companies('ZZO0VF1')
+    time.sleep(2)
+    ig_bot.choose_categories()
+    time.sleep(10)
 except:
     ig_bot.close()
