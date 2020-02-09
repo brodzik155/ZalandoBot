944f46ae5136f11acad2b766b9e0f3e7274c6d31
Student235281 <235281@student.edu.wroc.pl>
2020-02-09 19:54:53 +0100

    poprawki i rozpoczecie funkcji kategorie

--- ZalandoBot/Bot.py
+++ ZalandoBot/Bot.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
-import time
+from time import sleep
 
 categories = '//*[@id="inner-wrapper"]/section/div[2]/nav/a[1]/div'
 credentials_validate = '//*[@id="react-root-form"]/div/div/div[4]/div/form/fieldset/button'
 passwd = '//*[@id="form-password"]'
 log = '//*[@id="form-email"]'
 max_price_value = '200'
+
+
 class ZalandoBot:
 
     def __init__(self, username, password):
@@ -29,8 +31,8 @@ class ZalandoBot:
         password.send_keys(self.password)
         submit = self.driver.find_element_by_xpath(credentials_validate)
         submit.click()
-        time.sleep(1)
-        self.driver.find_element_by_xpath('//*[@id="mainNavWrapper"]/div[2]/div[2]').click()
+        sleep(1)
+        self.driver.find_element_by_xpath('//*[@id="inner-wrapper"]/div[1]/section/section/div').click()
 
     def open_companies(self, campagnies_id):
         self.driver.get('https://www.zalando-lounge.pl/campaigns/'+campagnies_id+'/all')
@@ -40,24 +42,23 @@ class ZalandoBot:
 
     def choose_categories(self):
         self.driver.find_element_by_xpath(categories).click()
-        time.sleep(1)   #Odtad nie działa....
-        self.driver.find_element_by_xpath('//*[@id="inner-wrapper"]/section/div[2]/div/div/div[1]/ul/li[2]/ul/li[1]').click()
-        time.sleep(2)
+        sleep(1)
+        self.driver.find_elements_by_xpath("//*[contains(text(), 'koszulki')]")[1].click()
 
     def price(self):
         self.driver.find_element_by_xpath('//*[@id="inner-wrapper"]/section/div[2]/nav/a[5]/div').click()
-        time.sleep(0.5)
+        sleep(0.5)
         input_field = self.driver.find_element_by_id('price-max')
         self.driver.execute_script("arguments[0].value = ''", input_field)
         input_field.send_keys(max_price_value)
         input_field.send_keys(Keys.RETURN)
 
-ig_bot = ZalandoBot('nasze.zdjecia1997@gmail.com', 'kubekpatryk')
 
-ig_bot.login()
-time.sleep(2)
-ig_bot.open_companies('ZZO0V48')
-time.sleep(2)
-#ig_bot.choose_categories()
-ig_bot.price()
-time.sleep(10)
+zalando_bot = ZalandoBot('nasze.zdjecia1997@gmail.com', 'kubekpatryk')
+
+zalando_bot.login()
+sleep(2)
+zalando_bot.open_companies('ZZO0V48')
+sleep(2)
+zalando_bot.choose_categories()
+sleep(10)
